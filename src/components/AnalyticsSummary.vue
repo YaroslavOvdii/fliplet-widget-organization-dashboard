677bc64e638b43b586ecf7e4000b769d6382cb75
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-22 09:55:35 +0300

    'analytics'

---
+++ src/components/AnalyticsSummary.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="analytics-container">
+    <SummaryCell v-for="(item, index) in transformedData" :data="item" :description="cellInformation[index]" :key="index"></SummaryCell>
+  </div>
+</template>
+
+<script>
+import SummaryCell from './SummaryCell.vue';
+
+export default {
+  data() {
+    return {
+      cellInformation: ['Studio sessions', 'Total users', 'New studio users', 'Apps created', 'Apps edited', 'Apps published'],
+      transformedData: undefined
+    };
+  },
+  props: {
+    analyticsData: Object
+  },
+  created() {
+    this.transformedData = Object.values(this.analyticsData);
+  },
+  components: {
+    SummaryCell
+  },
+  name: 'AnalyticsSummary'
+};
+</script>
---
+++ src/components/SummaryCell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="summary-cell">
+    <h2>{{ data.count }}</h2>
+    <small v-if="periodIncrease" class="up">(&#8593;X%)</small>
+    <small v-else class="down">(&#8595;X%)</small>
+    <small>{{ data.previousPeriodCount }} previous period</small>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      periodIncrease: true
+    };
+  },
+  props: {
+    description: String,
+    data: Array
+  },
+  mounted() {
+    if (this.data.previousPeriodCount > this.data.count) {
+      this.periodIncrease = false;
+    }
+  },
+  name: 'SummaryCell'
+};
+</script>
