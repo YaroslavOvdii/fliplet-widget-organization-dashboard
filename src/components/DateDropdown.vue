3846510d0fcee1c953a7c9f26f7223fcd17061a8
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-22 23:21:18 +0300

    'added-date-selection'

---
+++ src/components/DateDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <select v-model="selectedRange" name="dateRange" id="dateRange" class="date-dropdown form-control">
+    <option value="24hours">Last 24 hours</option>
+    <option value="7days">Last 7 days</option>
+    <option value="30days">Last 30 days</option>
+    <option value="90days">Last 90 days</option>
+    <option value="6months">Last 6 months</option>
+    <option value="12months">Last 12 months</option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedRange: '30days'
+    };
+  },
+  name: 'DateDropdown'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DateSelection.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="date-selection">
+    <DateDropdown></DateDropdown>
+    <date-range-picker
+      ref="picker"
+      opens="left"
+      :locale-data="dateFormat"
+      :autoApply=true
+      :ranges=false
+      v-model="dateRange"
+      @update="updateValues"
+      :linkedCalendars=false
+    >
+      <template v-slot:input="picker" style="min-width: 350px;">
+        {{ picker.startDate }} - {{ picker.endDate }}
+      </template>
+    </date-range-picker>
+  </div>
+</template>
+
+<script>
+import DateRangePicker from 'vue2-daterange-picker';
+import DateDropdown from './DateDropdown.vue';
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css';
+
+export default {
+  data() {
+    let startDate = new Date();
+    let endDate = new Date();
+    startDate.setDate(endDate.getDate() - 30);
+
+    return {
+      dateRange: {
+        startDate,
+        endDate
+      },
+      dateFormat: {
+        format: 'dd/mm/yyyy,
+        separator: ' - ',
+        monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      }
+    };
+  },
+  methods: {
+    updateValues() {
+
+    }
+  },
+  components: {
+    DateDropdown,
+    DateRangePicker
+  },
+  name: 'DateSelection'
+};
+</script>
