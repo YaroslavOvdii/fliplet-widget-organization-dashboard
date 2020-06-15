be9c6e0634ad43e0135a560c4eb0c8fe675d33b3
Yaroslav Ovdii <yaroslav.o@upplabs.com>
2020-06-15 16:36:56 +0300

    change how we get organization ID

--- dist/app.js
+++ dist/app.js
@@ -14912,7 +14912,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
-      perсent: 0
+      perсent: 0,
+      organizationId: 0
     };
   },
   props: {
@@ -14936,16 +14937,14 @@ __webpack_require__.r(__webpack_exports__);
       return moment(date).format('D MMM YYYY');
     },
     openUserProfile: function openUserProfile(options) {
-      Fliplet.Organizations.get().then(function (organizations) {
-        Fliplet.Studio.emit('overlay', {
-          name: 'edit-organization-user',
-          options: {
-            size: 'large',
-            title: 'Edit User',
-            userId: options.userId,
-            organizationId: organizations[0].id
-          }
-        });
+      Fliplet.Studio.emit('overlay', {
+        name: 'edit-organization-user',
+        options: {
+          size: 'large',
+          title: 'Edit User',
+          userId: options.userId,
+          organizationId: this.organizationId
+        }
       });
     },
     openAppAnalytics: function openAppAnalytics(options) {
@@ -14969,6 +14968,10 @@ __webpack_require__.r(__webpack_exports__);
   mounted: function mounted() {
     if (this.cellType === 'dynamic') {
       this.perсent = Object(_services_analytics__WEBPACK_IMPORTED_MODULE_0__["calculateDynamic"])(this.cellValue[0], this.cellValue[1]);
+    }
+
+    if (this.cellType === 'action') {
+      this.organizationId = Fliplet.Env.get('organizationId');
     }
   }
 });
--- src/components/tables/DataTableCell.vue
+++ src/components/tables/DataTableCell.vue
@@ -26,7 +26,8 @@ import { calculateDynamic } from '../../services/analytics';
 export default {
   data() {
     return {
-      perсent: 0
+      perсent: 0,
+      organizationId: 0
     };
   },
   props: {
@@ -50,18 +51,15 @@ export default {
       return moment(date).format('D MMM YYYY');
     },
     openUserProfile: function(options) {
-      Fliplet.Organizations.get()
-        .then(organizations => {
-          Fliplet.Studio.emit('overlay', {
-            name: 'edit-organization-user',
-            options: {
-              size: 'large',
-              title: 'Edit User',
-              userId: options.userId,
-              organizationId: organizations[0].id
-            }
-          });
-        });
+      Fliplet.Studio.emit('overlay', {
+        name: 'edit-organization-user',
+        options: {
+          size: 'large',
+          title: 'Edit User',
+          userId: options.userId,
+          organizationId: this.organizationId
+        }
+      });
     },
     openAppAnalytics: function(options) {
       Fliplet.Studio.emit('overlay', {
@@ -84,6 +82,10 @@ export default {
   mounted: function() {
     if (this.cellType === 'dynamic') {
       this.perсent = calculateDynamic(this.cellValue[0], this.cellValue[1]);
+    }
+
+    if (this.cellType === 'action') {
+      this.organizationId = Fliplet.Env.get('organizationId');
     }
   }
 };
